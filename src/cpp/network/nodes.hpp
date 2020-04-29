dbed9f9838ea57f986376635c235f51f6db55cd4
prakashupes <85prakash2017@gmail.com>
2020-04-29 09:42:11 +0530

    node

--- src/cpp/network/nodes.hpp
+++ src/cpp/network/nodes.hpp
@@ -1,9 +1,18 @@
 
 #include "Packates.hpp"
+//#include "../routing/RoutingProtocol.hpp" //To define table structure
+#include "../routing/TableStruct.hpp"
 #include<queue>
-#include <map>
+#include <vector>
 #ifndef NODE
 #define NODE
+struct Flowrule
+{
+	int nextHope,relay,ttl,cost;
+	Flowrule(int a, int b, int c,int d): nextHope(a),relay(b) ,ttl(c),cost(d){}
+
+
+};
 
 
 class Node
@@ -11,9 +20,8 @@ class Node
 
     public:
     int Node_id;
-   // int data; //char queue  p p2 p
-   // int weight;
-    map<int,int> Routing_Table;
+   
+   vector<table_attributes> flow_rule;
     int range=100;
     Packet tempPacket;//Node packet to temp //During transmision
     
