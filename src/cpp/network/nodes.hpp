5c19e7b85fc09edd522a0350e4b0762b77f58041
prakashupes <85prakash2017@gmail.com>
2020-05-06 13:57:21 +0530

    new flow rule structure according to msg id

--- src/cpp/network/nodes.hpp
+++ src/cpp/network/nodes.hpp
@@ -4,8 +4,10 @@
 #include "../routing/TableStruct.hpp"
 #include<queue>
 #include <vector>
+#include <map>
 #ifndef NODE
 #define NODE
+/*
 struct Flowrule
 {
 	int nextHope,relay,ttl,cost;
@@ -14,14 +16,14 @@ struct Flowrule
 
 };
 
-
+*/
 class Node
 {
 
     public:
     int Node_id;
    
-   vector<table_attributes> flow_rule;
+   map<string ,vector<table_attributes>> flow_rule;
     int range=100;
     Packet tempPacket;//Node packet to temp //During transmision
     
