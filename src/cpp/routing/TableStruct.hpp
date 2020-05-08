d78ec558dc9f50c7a439d8b3def0758bbe07d351
prakashupes <85prakash2017@gmail.com>
2020-05-08 16:10:52 +0530

    final routin table struct

--- src/cpp/routing/TableStruct.hpp
+++ src/cpp/routing/TableStruct.hpp
@@ -2,8 +2,9 @@
 #define TABLESTRUCT
 struct table_attributes
 {
-	int nextHope,relay,ttl,cost;
-	table_attributes (int a, int b, int c,int d): nextHope(a),relay(b) ,ttl(c),cost(d){}
+	int nextHope,ttl,cost;
+	float relability;
+	table_attributes (int a, float b, int c,int d): nextHope(a),relability(b) ,ttl(c),cost(d){}
 
 };
 #endif
